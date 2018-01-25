a5b0ff37d7c15d579df5979257b75abb060e9e65
Michela Zucca <michela.zucca@heig-vd.ch>
2018-01-25 13:10:17 +0100

    new version

--- app/modules/profile/profileCtrl.js
+++ app/modules/profile/profileCtrl.js
@@ -10,11 +10,11 @@
 	*/
 
   	angular
-		.module('profile')
+		.module('users-and-teams')
 		.controller('ProfileCtrl', Profile);
 
 		Profile.$inject = [];
-
+		
 		/*
 		* recommend
 		* Using function declarations
@@ -27,11 +27,21 @@
 
 			vm.showUpdate = false;
 
-			vm.profile = {"id":2,
+			vm.profile = {
+				"id":2,
 				username:"micky",
 				firstName:"lolo",
 				lastName:"chchca",
-				email:"michela.zucca@lala.ch"
+				email:"michela.zucca@lala.ch",
+			};
+
+			vm.profileUpdate = {
+				username:'',
+				firstName:'',
+				lastName: '',
+				email:'',
+				password: '',
+				profil_password:'',
 			};
 
 			vm.listTeams = [
@@ -65,13 +75,33 @@
 			];
 
 			vm.teamSelected = '';
+
 			vm.update = function(){
+				vm.profileUpdate.firstName = vm.profile.firstName;
+				vm.profileUpdate.lastName = vm.profile.lastName;
+				vm.profileUpdate.email = vm.profile.email;
+				vm.profileUpdate.username = vm.profile.username;
+
 				vm.showUpdate = true;
 			}
 
 			vm.selectTeam = function(){
 				const team = vm.selectTeam;
 				
+			}
+
+			vm.putUpdate = function(){
+				console.log(vm.profileUpdate);
+				if(vm.profileUpdate.username != '' && 
+				vm.profileUpdate.password != '' &&
+				vm.profileUpdate.email != '' && 
+				vm.profileUpdate != '' &&
+				vm.profileUpdate != '' &&
+				vm.profileUpdate.confirm_password === vm.profileUpdate.password){
+					console.log('update form');
+					vm.showUpdate = false;
+				}
+
 			}
 
 		}
